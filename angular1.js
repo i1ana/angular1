47343d06a858d55e18908e8ed1aa57efa59ed1db
Student <student@RU-007.local>
2016-01-20 14:23:05 -0700

    buttons done

--- angular1.js
+++ angular1.js
@@ -1,14 +1,24 @@
+// --------Part 1--------------
+
 angular.module('button', [])
 
 angular
 	.module('button')
  		.controller("buttonClick", ['$scope', function($scope){
  				$scope.clickMe = function(){
- 					// console.log("butts")
- 				}
+ 					console.log("butts")
+ 					$scope.surprise = !$scope.surprise
 
- 				$scope.listClick = function(){
- 					
  				}
+ 					$scope.surprise = false; 
+
+ 					$scope.listClick = function(){
+ 						$scope.hideTwo = !$scope.hideTwo
+ 					}
+ 							$scope.hideTwo = true; 
 
  	}])
+
+
+
+
