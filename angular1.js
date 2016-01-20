ee34a5e0e534369581c5574bb7bbb5820994a818
Student <student@RU-007.local>
2016-01-20 12:31:32 -0700

    lunch time

--- angular1.js
+++ angular1.js
@@ -1,1 +1,14 @@
 angular.module('button', [])
+
+angular
+	.module('button')
+ 		.controller("buttonClick", ['$scope', function($scope){
+ 				$scope.clickMe = function(){
+ 					// console.log("butts")
+ 				}
+
+ 				$scope.listClick = function(){
+ 					
+ 				}
+
+ 	}])
