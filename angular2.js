0273ad3f90b959a0d914cefb7fb6da93fd369ebc
Student <student@RU-007.local>
2016-01-20 17:21:14 -0700

    end of day

--- angular2.js
+++ angular2.js
@@ -3,12 +3,21 @@ angular.module('pink', [])
 angular.module('pink')
 	.controller('bacon', [$scope, function($scope){
 		
-		$scope.colorChange = function(){
+		// $scope.mouseOver= function(){
+		// 	$scope.mouseOver=
+
+		$scope.exclaimation = !$scope.exclaimation		
+	}
+
+
+	$scope.exclaimation= false 
+
+	$scope.link = function(){
+		prompt("Do you want to click this link?")
+		if("cancel"){
 			
-
 		}
-
-
+	}
 
 	}])
 
