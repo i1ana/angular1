a963e1959931eb80f27bba0962525c24ec9e36b5
Student <student@RU-007.local>
2016-01-21 17:04:54 -0700

    solved

--- angular2.js
+++ angular2.js
@@ -2,25 +2,31 @@ angular.module('pink', [])
 	.controller('bacon', ['$scope', function($scope){
 		
 		$scope.switch = false
+		$scope.punctuation = "."
+		$scope.nope = true;
 
 		$scope.mouseEnter = function(){
 			$scope.switch = true
-
-		// $scope.exclaimation = !$scope.exclaimation	
+			$scope.punctuation = "!!!!"
+		}
 
 		$scope.mouseLeave = function(){
 			$scope.switch = false
 		}	
-	}
+	
+		$scope.exclaimation = function() {
+			$scope.punctuation = "!!!!"
+		}
 
-
-	// $scope.exclaimation= false 
-
-	// $scope.link = function(){
-	// 	var input = prompt("Do you want to click this link?")
-	// 	if(input === true){
-	// 		$scope.hideLink ==
-	// 	}
-
+		$scope.hideLink = function(event){
+			var input = confirm("Do you want to click this link?")
+		 	if(input === false){
+		 		// if( !confirm("Do you want to click this link?")){
+				$scope.nope = false;
+				event.preventDefault()
+				
+			}
+		}
 	}])
+
 
